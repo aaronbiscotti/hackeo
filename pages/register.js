59cbe72dcf047c47f5e98214c4be66a299540a3c
adamksultan <sultana@bxscience.edu>
2022-08-13 16:03:22 -0400

    Registration

---
+++ pages/register.js
@@ -0,0 +1,18 @@
+import Link from "next/link"
+const Page = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex flex-col items-center space-y-8">
+                <h1 className="text-4xl">Register</h1>
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Password" />
+                <button className='btn'>
+                    Register
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Page
