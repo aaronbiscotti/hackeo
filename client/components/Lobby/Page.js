0bb7e060c0c33a49803fd2ed90486020aec2260d
aaronbiscotti <thesoupmanguy@gmail.com>
2022-08-13 18:17:30 -0400

    rooms

--- client/components/Lobby/Page.js
+++ client/components/Lobby/Page.js
@@ -1,27 +1,21 @@
 import Link from 'next/link'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import io from "socket.io-client"
 const socket = io.connect("http://localhost:3001");
 
 
 const Page = () => {
-    const sendMessage = () => {
-        socket.emit("send_message", { message: "Hello" });
-    }
-
-    useEffect(() => {
-        socket.on("receive_message", (data) => {
-            alert(data.message);
-        })
-    }, [socket])
+    const [id, setId] = useState("");
     return (
         <div className="flex justify-center items-center h-screen">
             <div className="flex flex-col items-center space-y-8">
                 <h1 className="text-4xl">Play</h1>
-                <input type="text" placeholder="Room code" />
-                <button className="btn" onClick={sendMessage}>
-                    Play
-                </button>
+                <input type="text" placeholder="Room code" onChange={(e) => setId(e.target.value)} />
+                <Link href={`/room/${id}`}>
+                    <button className="btn">
+                        Play
+                    </button>
+                </Link>
                 <h1 className="text-light font-semibold">OR</h1>
                 <Link href="/create-room">
                     <button className="btn">
