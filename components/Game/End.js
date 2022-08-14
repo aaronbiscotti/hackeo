3be81734a79db3d31acf35370c38b7ce861428b7
aaronbiscotti <thesoupmanguy@gmail.com>
2022-08-14 10:34:40 -0400

    switch up

--- components/Game/End.js
+++ components/Game/End.js
@@ -8,7 +8,7 @@ const End = () => {
     return (
         <div className="absolute top-0 bottom-0 right-0 left-0 bg-[#191B34] flex justify-center items-center flex-col space-y-10">
             <h1 className="text-3xl">You finished with a score of {score}!</h1>
-            <Link href="/register">
+            <Link href="/">
                 <button className="btn">Play again</button>
             </Link>
         </div>
